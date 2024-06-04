5d6092fbdcc25e6bab65713efc81010c292453c3
akmindaugas <mindaugas.akelis@gmail.com>
2024-06-04 23:09:18 +0300

    commit some dev

--- LanguageSelector.jsx
+++ LanguageSelector.jsx
@@ -12,22 +12,68 @@ const languages = [
 
 
 const customStyles = {
-  control: (provided) => ({
-    ...provided,
-    padding: '0px' ,
-    borderRadius: '5px',
-    border: '2px solid #000000',
-    opacity: 0.2,
-    color: 'rgb(238, 235, 235)',
-    
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    backgroundColor: state.isFocused ? '#rgb(238, 235, 235)' : '#000000',
-    color: 'rgb(238, 235, 235)',
-    borderRadius: '5px',
-  }),
-};
+        control: (provided) => ({
+        ...provided,
+        color:  'rgb(238, 235, 235)',
+        backgroundColor: '#000000',
+        padding: '0px',
+        borderRadius: '6px',
+        border: '2px solid #494949',
+        boxShadow: 'none',
+        '&:hover': {
+          transition: '0.6s',
+          border: '2px solid #F5F5F5',
+          borderRadius: '12px',
+          color: 'white',
+          backgroundColor: '#000000',
+        },
+      }),
+      option: (provided, state) => ({
+        ...provided,
+        padding: '2px',
+        margin: 'auto',
+        border: '2px solid #494949',
+        borderRadius: '6px',
+        backgroundColor: state.isSelected ? '#000000' : 'rgb(238, 235, 235)',
+        color: state.isSelected ? '#fff' : '#333',
+        '&:hover': {
+          backgroundColor: '#f0f0f0',
+          border: '2px solid #F5F5F5',
+          borderRadius: '12px',
+        },
+      }),
+      menu: (provided) => ({
+        ...provided,
+        borderRadius: '6px',
+        marginTop: '0px',
+        '&:hover': {
+          borderRadius: '12px',
+          color: 'white',
+        },
+      }),
+      // faceview properties
+      singleValue: (provided) => ({
+        ...provided,
+        color: '#EEEBEB',
+      }),
+      // arrow element
+      placeholder: (provided) => ({
+        ...provided,
+        color: 'rgb(238, 235, 235)',
+        '&:hover': {
+                    color: 'white',
+        },
+      }),
+      valueContainer: (provided) => ({
+        ...provided,
+        padding: '0px 2px',
+        
+      }),
+      multiValue: (provided) => ({
+        ...provided,
+        backgroundColor: 'red',
+      }),
+    };
 
 const LanguageSelector = ({ onLanguageChange }) => {
   const handleChange = (selectedOption) => {
